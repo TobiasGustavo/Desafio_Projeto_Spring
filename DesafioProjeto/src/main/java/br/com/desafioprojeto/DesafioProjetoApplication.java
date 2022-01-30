7b224cfc921f5e96476b8c512e2e67e0a11d1ed3
tgzss <tobiasgustavoh@gmail.com>
2022-01-30 18:00:25 -0300

    Commit Initial
    Desafio DIO _Projeto Spring

---
+++ DesafioProjeto/src/main/java/br/com/desafioprojeto/DesafioProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafioprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProjetoApplication.class, args);
+	}
+
+}
